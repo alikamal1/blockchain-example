dbb5c2e9e41a96421b4615377c13375ecf10d6f7
Ali Kamal <alikamal1@yahoo.com>
2020-02-28 14:36:30 +0300

    validiation

--- main.js
+++ main.js
@@ -18,21 +18,44 @@ class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
     }
+
     createGenesisBlock() {
         return new Block(0, '01/01/2020', 'Genesis block', '0');
     }
+
     getLatestBlock() {
-        return this.chain[this.chain.length -1];
+        return this.chain[this.chain.length - 1];
     }
+
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 let bitCoin = new Blockchain();
-bitCoin.addBlock(new Block(1, '10/07/2020', { amount: 4}));
-bitCoin.addBlock(new Block(2, '1/9/2020', {amount : 10}));
+bitCoin.addBlock(new Block(1, '10/07/2020', { amount: 4 }));
+bitCoin.addBlock(new Block(2, '1/9/2020', { amount: 10 }));
+console.log(JSON.stringify(bitCoin, null, 4));
 
-console.log(JSON.stringify(bitCoin));
+console.log('is blockchain valid? ' + bitCoin.isChainValid());
+
+bitCoin.chain[1].data = { amount: 100};
+console.log('is blockchain valid? ' + bitCoin.isChainValid());
+bitCoin.chain[1].hash = bitCoin.chain[1].calculateHash();
+console.log('is blockchain valid? ' + bitCoin.isChainValid());
